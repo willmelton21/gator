8b8a9c60fcd2296529cf8744c3577c1be7625a73
WillWSL <meltonjw3@gmail.com>
2025-03-28 11:15:58 -0500

    added log user command

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,14 +6,59 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"errors"
 )
 
 type Config struct  {
 	DbUrl	 string `json:"db_url"`
-	currUserName	 string `json:"current_user_name"`
+	CurrUserName	 string `json:"current_user_name"`
+}
+
+type State struct {
+	ConfPtr *Config
+}
+
+type Command struct {
+	CommandName string
+	Args [] string
+}
+
+type Commands struct {
+	Handlers map[string]func(*State,Command) error //map of command names to their handler functiosn
 }
 
 const configFileName = "/.gatorconfig.json"
+
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	//registers a new handler function for a command name
+	c.Handlers[name] = f
+	return 
+}
+
+
+func (c *Commands) Run(s *State, cmd Command) error {
+	//runs a given command with the provided state if it exists
+	cmdHandler, ok := c.Handlers[cmd.CommandName]
+	if !ok {
+		return errors.New("error was not found in commands map")
+	}
+	return cmdHandler(s,cmd)
+}
+
+func HandlerLogin(s *State, cmd Command) error {
+	fmt.Println("in handlerlogin")
+	if len(cmd.Args) <= 0 {
+		return fmt.Errorf("username required for login command") 
+	}
+	err := s.ConfPtr.SetUser(cmd.Args[0])
+	if err != nil {
+		return err 
+	}
+	fmt.Println("user has been set to %s\n", cmd.Args[0])
+	return nil
+}
+
+
 
 func getConfigFilePath() (string, error) {
 
@@ -58,8 +103,8 @@ func Read() (Config, error) {
 func (conf *Config) SetUser(user string) error {
 	
 
-	conf.currUserName = user
-	fmt.Println("user is now ",conf.currUserName)
+	conf.CurrUserName = user
+	fmt.Println("user is now ",conf.CurrUserName)
 	return write(*conf)
 }
 
--- main.go
+++ main.go
@@ -3,22 +3,45 @@ package main
 import (
 	"fmt"
 	"github.com/willmelton21/gator/internal/config"
+	"os"
 )
 
 func main() {
 
 	conf,err := config.Read()
-
+	
 	if err != nil {
 		fmt.Println("err from read is :",err)
 	}
 
-	conf.SetUser("Will")
-	newConf,err := config.Read()
-	if err != nil {
-		fmt.Println("err from read is :",err)
+	state := &config.State{ConfPtr: &conf}
+	cmds := &config.Commands{
+		Handlers: make(map[string]func(*config.State, config.Command) error),
+	}
+        
+	cmds.Register("login",config.HandlerLogin)
+
+	args := os.Args
+	if (len(args) < 2) {
+		fmt.Println("Error: Not enough arguments. Usage: gator <command> [args]")		
+		os.Exit(1)
 	}
 
-	fmt.Printf("%+v\n", newConf)
+	commandName := args[1]
 
+	var commandArgs []string
+	if len(args) > 2 {
+		commandArgs = args[2:]
+	}
+	cmd := config.Command {
+		CommandName: commandName,
+		Args: commandArgs,
+	}
+   	fmt.Println("args are ",args[2:])	
+	err = cmds.Run(state,cmd)
+	if err != nil {
+
+		fmt.Println("Error:",err)
+		os.Exit(1)
+	}
 }
